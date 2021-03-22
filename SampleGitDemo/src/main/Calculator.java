ae27559aa3d05c1e22fbf012d031238492080e23
alwyr <alwyr@DESKTOP-9J8HL14>
2021-03-22 18:27:30 +0530

    Sub(20,10)

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -4,10 +4,11 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
+		sub(20,10);
 	}
 	public static void sub(int num1,int num2){
 	      System.out.println(num1-num2);
+	      
 	  }
 
 }
