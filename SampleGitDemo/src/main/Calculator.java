b148d381de7ad5ea9ff84d474c8ae4c5a65ea67c
alwyroy0725 <81158471+alwyroy0725@users.noreply.github.com>
2021-03-22 18:30:52 +0530

    Add Function

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -10,5 +10,9 @@ public class Calculator {
 	      System.out.println(num1-num2);
 	      
 	  }
+	public static void add(int num1,int num2){
+	      System.out.println(num1+num2);
+	      
+	  }
 
 }
